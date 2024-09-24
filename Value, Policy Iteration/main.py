05f9b5fb779251086c8dfa17942ee7fc871d379d
Swift <s87217647@gmail>
2024-09-23 18:58:59 -0700

    Mark

---
+++ Value, Policy Iteration/main.py
@@ -0,0 +1,34 @@
+from mymdp import MDP
+from mypa2_dp import ValueAgent, PIAgent, VIAgent
+
+
+if __name__ == '__main__':
+
+    mdp = MDP("./mdp1.json")
+
+    zero_val_states = {'0': 0, '1': 0, '2': 0}
+
+    va = ValueAgent(mdp)
+    print(va.computeq_fromv(zero_val_states))
+    #al_states))
+
+
+    # pia = PIAgent(mdp)
+    # print(pia.policy_iteration())
+    # for x in pia.v_update_history:
+    #     print(x)
+    #
+    #
+    # via = VIAgent(mdp)
+    # print(via.value_iteration())
+    # for x in via.v_update_history:
+    #     print(x)
+
+
+
+
+
+    print("This is the end, hold your breath and cout to ten")
+
+
+
--- Value, Policy Iteration/mian.py
+++
@@ -1,9 +0,0 @@
-from mymdp import MDP
-from mypa2_dp import PIAgent, ValueAgent
-
-
-if __name__ == '__main__':
-    print("hello world!")
-
-    mdp = MDP("./mdp1.json")
-
